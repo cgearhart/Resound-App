a99cf4d69fc0dd099968f36e535a46d0f7799015
CGearhart <cgearhart@gmail.com>
2014-12-27 01:06:29 -0500

    Front end rewrite
      - Use single page app (avoids requiring mic access reauth unless page is refreshed)
      - Add notifications & animations
      - Eliminate unused Jinja templates

--- ResoundApp/js/audio.js
+++ ResoundApp/js/audio.js
@@ -2,10 +2,11 @@
 var audio_context;
 var mediaStream;
 
-var CLIP_LENGTH = 7000;  // length of recorded audio clip (in ms)
+var CLIP_LENGTH = 8000;   // length of recorded audio clip (in ms)
 var TIMEOUT     = 20000;  // POST response timeout (in ms)
 
 function setupAudio() {
+    showResult("Please allow microphone access - note that mobile browsers are not currently supported.", 8000);
     try {
         // webkit & mozilla shim
         window.AudioContext = window.AudioContext || window.webkitAudioContext;
@@ -16,22 +17,22 @@ function setupAudio() {
         console.log('Audio context set up.');
     } catch (e) {
         alert('No web audio support in this browser!');
+        showResult("Sorry!<br>Your browser is not supported.");
     }
     
     navigator.getUserMedia({audio: true},
         function (stream) {
             mediaStream = audio_context.createMediaStreamSource(stream);
             console.log('Media stream created.');
+            document.getElementById("button").classList.toggle('disabled');
+            showResult("");
         },
         function(e) { console.log('No live audio input: ' + e);
+        showResult("Sorry!<br>Your microphone is unavailable.");
     });
 }
 
 function recordAudioClip(buttonDiv) {
-    /*
-    *  Record a 10 second audio clip from the microphone and send the contents
-    *  as a file blob in a POST request to the webserver backend
-    */
 
     if (!mediaStream) return;  // abort if the stream hasn't been established
     
@@ -43,41 +44,89 @@ function recordAudioClip(buttonDiv) {
     buttonDiv.style.pointerEvents = 'none';
     buttonDiv.classList.toggle('disabled');
     
-    // TODO: activate animations
+    // Enable message display & activate spinner animation
+    showDialog("Recording");
+    showResult("");
     
-    setTimeout(
-        function () {
-            console.log("Stop recording.");
-            recorder.stop();
-            buttonDiv.style.pointerEvents = 'auto';
-            buttonDiv.classList.toggle('disabled');
-            recorder.exportWAV(sendAudio);
-            recorder.clear();
-        },
-        CLIP_LENGTH);
+    setTimeout(function () {
+        console.log("Stop recording.");
+        recorder.stop();
+        buttonDiv.style.pointerEvents = 'auto';
+        buttonDiv.classList.toggle('disabled');
+        console.log("Convert to WAV & upload.");
+        recorder.exportWAV(sendAudio);
+        recorder.clear();
+    }, CLIP_LENGTH);
 }
 
 function sendAudio(blob) {
-    /*
-    *  Make an HTTP POST request including a file blob in a POST request to 
-    *  the webserver backend for identification
-    */
-    console.log("Convert to WAV & upload.");
+    
     var request = new XMLHttpRequest();
+    var result_div = document.getElementById("result");
+
+    showDialog("Processing");
+
     request.onload = function() {
-        console.log("Request finished.");
+        
+        console.log("Request complete.");
         console.log(request.responseText);
-        if (request.status === 200) {
-            window.location.href = window.location.origin + "/id/" + request.responseText;
+        
+        disableDialog();
+
+        if (request.status != 200) {
+            console.log("Bad response code: " + request.status);
+            showResult("There was a problem!<br>Please try again.");
         } else {
-            console.log("Error!\n" + request.responseText);
+            try {
+                response = JSON.parse(request.responseText);
+                var message = "Best Match:<hr>" +
+                              response.artist + " - " + response.title + "<br>" +
+                              "(" + response.year + ")";
+                showResult(message);
+            } catch (err) {
+                // write an error to the result div then fade it away after 8s
+                console.log("Error!\n" + err.message);
+                showResult("Sorry!<br>No Match Found.", 8000);
+            }
         }
+        
     };
     request.open("POST", "/id", true);
+    
     request.timeout = TIMEOUT;
     request.ontimeout = function () {
         console.log("Request timeout.");
-        // window.location.href = window.location.origin + "/id/";
+        disableDialog();
+        showResult("Request timed out.<br>Please try again.", 8000);
     };
+    
     request.send(blob);
+}
+
+function disableDialog() {
+
+    var dialog_div = document.getElementById("dialogBox");
+    dialog_div.classList.remove("visible");
+    dialog_div.classList.add("hidden");
+    document.getElementById("message").innerHTML = "";
+}
+
+function showDialog(message) {
+    
+    var dialog_div = document.getElementById("dialogBox");
+    dialog_div.classList.remove("hidden");
+    dialog_div.classList.add("visible");
+    document.getElementById("message").innerHTML = message;
+}
+
+function showResult(message, duration) {
+
+    var result_div = document.getElementById("result");
+    result_div.className = "fadein";
+    result_div.innerHTML = message;
+    if (arguments.length > 1) {
+        setTimeout(function() {
+            result_div.className = "fadeout";
+        }, duration);
+    }
 }
